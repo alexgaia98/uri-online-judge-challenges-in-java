dc9e0f9a31b34ba9b23086324e5501f259dca580
Alex <alex.gaia98@gmail.com>
2021-11-22 14:58:00 -0300

    initial Commit

---
+++ src/br/com/alexgaia98/challenges/HelloWord/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.com.alexgaia98.challenges.HelloWord;
+
+public class HelloWorld {
+ 
+    public static void main(String[] args) {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
