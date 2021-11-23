07164c9913b524d4a67628cf2697c79aa29726ca
Alex <alex.gaia98@gmail.com>
2021-11-23 19:01:37 -0300

    Add Problem 1555

---
+++ src/br/com/alexgaia98/uriproblems/matematic/Functions.java
@@ -0,0 +1,35 @@
+package br.com.alexgaia98.uriproblems.matematic;
+
+import java.util.Scanner;
+
+public class Functions {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int x, y, n; 
+        
+        int fRafael;
+        int fBeto;
+        int fCarlos;
+        
+        n = scanner.nextInt();
+        
+        for(int i=0;i<n;i++){
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            
+            fRafael = ((3*x)*(3*x))+(y*y);
+            fBeto = 2*(x*x)+((5*y)*(5*y));
+            fCarlos = (-100*x)+(y*y*y);
+            
+            if ((fRafael>fBeto)&&(fRafael>fCarlos)){               
+                    System.out.println("Rafael ganhou");               
+            } else if (fBeto>fCarlos){
+                System.out.println("Beto ganhou");
+            } else {
+                System.out.println("Carlos ganhou");
+            }
+        }
+		
+	}
+}
