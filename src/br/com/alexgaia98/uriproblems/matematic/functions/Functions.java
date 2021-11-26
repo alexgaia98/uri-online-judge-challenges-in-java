02a4bd9d3d218f49be188302eca03d1db2d9e847
Alex Silva <43093680+alexgaia98@users.noreply.github.com>
2021-11-26 17:25:07 -0300

    Code refactoring

--- src/br/com/alexgaia98/uriproblems/matematic/functions/Functions.java
+++ src/br/com/alexgaia98/uriproblems/matematic/functions/Functions.java
@@ -6,30 +6,31 @@ public class Functions {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-        int x, y, n; 
+		
+		int x, y, n; 
+
+		int fRafael;
+		int fBeto;
+		int fCarlos;
         
-        int fRafael;
-        int fBeto;
-        int fCarlos;
-        
-        n = scanner.nextInt();
-        
-        for(int i=0;i<n;i++){
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            
-            fRafael = ((3*x)*(3*x))+(y*y);
-            fBeto = 2*(x*x)+((5*y)*(5*y));
-            fCarlos = (-100*x)+(y*y*y);
-            
-            if ((fRafael>fBeto)&&(fRafael>fCarlos)){               
-                    System.out.println("Rafael ganhou");               
-            } else if (fBeto>fCarlos){
-                System.out.println("Beto ganhou");
-            } else {
-                System.out.println("Carlos ganhou");
-            }
-        }
+		n = scanner.nextInt();
+
+		for(int i=0;i<n;i++){
+		    x = scanner.nextInt();
+		    y = scanner.nextInt();
+
+		    fRafael = ((3*x)*(3*x))+(y*y);
+		    fBeto = 2*(x*x)+((5*y)*(5*y));
+		    fCarlos = (-100*x)+(y*y*y);
+
+		    if ((fRafael>fBeto)&&(fRafael>fCarlos)){               
+			    System.out.println("Rafael ganhou");               
+		    } else if (fBeto>fCarlos){
+			System.out.println("Beto ganhou");
+		    } else {
+			System.out.println("Carlos ganhou");
+		    }
+		}
 		
 	}
 }
